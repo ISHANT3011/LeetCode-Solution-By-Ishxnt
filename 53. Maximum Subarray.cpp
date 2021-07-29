18a90f2bdfdce1ce1c50293682f5496d722a2976
ISHANT3011 <59556882+ISHANT3011@users.noreply.github.com>
2021-07-29 15:18:26 +0530

    Add files via upload

---
+++ 53. Maximum Subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int mx = INT_MIN;
+        int sum = 0;
+        for(int i =0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            if(sum>mx)
+            {
+                mx=sum;
+            }
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        return mx;
+    }
+};
