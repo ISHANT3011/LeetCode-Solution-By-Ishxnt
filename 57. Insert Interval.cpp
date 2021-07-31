3127ac268733ca5c40dae7512160fa11e47ad131
ISHANT3011 <59556882+ISHANT3011@users.noreply.github.com>
2021-08-01 01:03:09 +0530

    Add files via upload

---
+++ 57. Insert Interval.cpp
@@ -0,0 +1,52 @@
+struct Interval
+{
+    int start,end;
+};
+bool mycomp(Interval a,Interval b)
+{
+    return a.start<b.start;
+}
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& i, vector<int>& newInterval) {
+        i.push_back(newInterval);
+          int n = i.size();
+        vector<Interval> arr(n);
+        for(int j=0;j<n;j++)
+        {
+            arr[j].start = i[j][0];
+            arr[j].end = i[j][1];
+        }
+        if(n<=1)
+        {
+            return i;
+        }
+        sort(arr.begin(),arr.end(),mycomp);
+        int index = 0;
+        for(int j=1;j<n;j++)
+        {
+            
+            if(arr[index].end >= arr[j].start )
+            {
+                arr[index].end = max(arr[j].end,arr[index].end);
+                arr[index].start = min(arr[index].start,arr[j].start);
+            }
+            else if(arr[index].end<arr[j].start)
+            {
+                index++;
+                arr[index] = arr[j];
+            }
+        }
+        i.clear();
+        
+        for(int j=0;j<=index;j++)
+        {
+            vector<int> curr;
+            curr.push_back(arr[j].start);
+            curr.push_back(arr[j].end);
+            i.push_back(curr);
+        }
+        return i;
+    }
+};
